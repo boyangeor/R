d52af6df66e3af2238a7a029116d98fa2ec2ed7a
Boyan Georgiev <boyan.georgiev@students.uni-mannheim.de>
2017-03-26 21:12:33 +0200

    LFM

--- func/func_loader.R
+++ func/func_loader.R
@@ -1,9 +1,12 @@
 func_loader <- function(path) {
   for(d in list.dirs(path))
     for (f in list.files(d, full.names = T))
-      # If the file has .R extension AND is not 'func_loader.R',
-      # otherwise recursion happens and R blocks
+      # If the file has .R extension AND does NOT contain "_nl"
+      # ("_nl" for files that should not be loaded by default) 
+      # is not 'func_loader.R', otherwise recursion happens and
+      # R blocks
       if (grepl(".*\\.R", basename(f), perl=T) &&
+          !grepl("_nl", basename(f), perl=T) &&
           basename(f) != "func_loader.R")
         source(f)
 }
